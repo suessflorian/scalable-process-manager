10b716e486a8328aa570ebb684fb8df9f38807c2
Florian Suess <floriansuess96@icloud.com>
2021-10-06 19:42:02 +1300

    Add lightweight GraphQL server

---
+++ go.mod
@@ -0,0 +1,10 @@
+module subscription
+
+go 1.16
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/graph-gophers/graphql-go v1.2.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gofrs/uuid"
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+	log "github.com/sirupsen/logrus"
+)
+
+type ProcessState string
+
+const PORT = 8080
+
+const (
+	RUNNING  ProcessState = "RUNNING"
+	FINISHED ProcessState = "FINISHED"
+	FAILED   ProcessState = "FAILED"
+)
+
+type Process struct {
+	Pid    pid
+	Status ProcessState
+}
+type resolver struct{}
+
+func (r *resolver) NewProcess() *Process {
+	return &Process{newPID(), RUNNING}
+}
+
+func (r *resolver) AllProcesses() []Process {
+	return nil
+}
+
+//go:embed schema.graphql
+var schema string
+
+func main() {
+	http.Handle("/query", &relay.Handler{
+		Schema: graphql.MustParseSchema(
+			schema,
+			&resolver{},
+			graphql.UseFieldResolvers(),
+		)})
+
+	if err := http.ListenAndServe(":8080", nil); err != http.ErrServerClosed {
+		log.WithError(err).Errorf("server closed due to unexpected error")
+	}
+}
+
+type pid uuid.UUID
+
+func (p pid) MarshalJSON() ([]byte, error) {
+	return strconv.AppendQuote(nil, uuid.UUID(p).String()), nil
+}
+
+func (p *pid) UnmarshalGraphQL(input interface{}) error {
+	var err error
+	switch input := input.(type) {
+	case string:
+		uuid, err := uuid.FromString(input)
+		if err != nil {
+			return err
+		}
+		*p = pid(uuid)
+	default:
+		err = fmt.Errorf("wrong type for ID: %T", input)
+	}
+	return err
+}
+
+func (pid) ImplementsGraphQLType(name string) bool {
+	return name == "ID"
+}
+
+func newPID() pid {
+	uuid, _ := uuid.NewV4()
+	return pid(uuid)
+}
